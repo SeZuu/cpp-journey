32c5968737993b87e1b6b84fcf57f772633c4c55
sezu <dukemlet@gmail.com>
2024-07-28 09:46:32 -0500

    01-exercises added

---
+++ 01-fundamentals/fundamentals-exercises/01-exercises.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+int main()
+{
+    /*Ejercicio 1
+    Una empresa constructora vende terrenos con la Forma A (ver figura). Elabore un programa en
+    C++ que permita obtener el área respectiva de un terreno de medidas de cualquier valo*/
+
+    // float a, b;
+    // std::cout << "Ingrese A: ";
+    // std::cin >> a;
+    // std::cout << std::endl;
+    // std::cout << "Ingrese B: ";
+    // std::cin >> b;
+    // std::cout << std::endl;
+
+    // float area_primera_figura = (b * (a / 2)) / 2;
+    // float area_segunda_figura = b * (a / 2);
+    // std::cout << "El area de la primera figura es: " << area_primera_figura << std::endl;
+    // std::cout << "El area de la segunda figura es: " << area_segunda_figura << std::endl;
+    // std::cout << "El area de la figura comleta es: " << area_primera_figura + area_segunda_figura;
+
+    /*Ejercicio 2
+    SEDAPAL requiere determinar el pago que debe realizar una persona por el total de metros
+    cúbicos que consume de agua al llenar una piscina móvil (ver figura). Elabore un programa en
+    C++ que permita determinar ese pago si se tiene como dato adicional el precio por m3
+    de agua*/
+
+    // float a, b, l, price;
+    // std::cout << "Ingrese A: ";
+    // std::cin >> a;
+    // std::cout << "Ingrese B: ";
+    // std::cin >> b;
+    // std::cout << "Ingrese L: ";
+    // std::cin >> l;
+    // std::cout << "Ingrese el precio por m3 de agua: ";
+    // std::cin >> price;
+
+    // std::cout << "El pago es: " << a * b * l * price;
+
+    /*Ejercicio 3
+    Carmela, Javier y Eulogio aportan cantidades de dinero para formar un capital. Elabore un
+    programa en C++ que permita determinar el capital total formado y el porcentaje de dicho
+    capital que aporta cada uno.
+    */
+
+    // float capital;
+    // float carmela, javier, eulogio;
+    // std::cout << "Aporte de Carmela: ";
+    // std::cin >> carmela;
+    // std::cout << "Aporte de Javier: ";
+    // std::cin >> javier;
+    // std::cout << "Aporte de Eulogio: ";
+    // std::cin >> eulogio;
+
+    // capital = carmela + javier + eulogio;
+
+    // std::cout << "El capital formado es: " << capital << std::endl;
+    // std::cout << "El porcentaje aportado por Carmela es: " << 100 * (carmela / capital) << "%" << std::endl;
+    // std::cout << "El porcentaje aportado por Javier es: " << 100 * (javier / capital) << "%" << std::endl;
+    // std::cout << "El porcentaje aportado por Eulogio es: " << 100 * (eulogio / capital) << "%";
+    // return 0;
+
+    /*Ejercicio 4
+    La repartición de ganancias en una empresa se hace en forma proporcional al número de
+    acciones de cada uno de sus tres socios. Dada la ganancia de un año y la cantidad de acciones
+    de cada socio, elabore un programa en C++ que determine el monto que le corresponde a cada
+    socio.*/
+
+    // float ganancia, pago_por_accion;
+    // int acciones_socio_1, acciones_socio_2, acciones_socio_3;
+
+    // std::cout << "Ingrese la ganancia en el anio: ";
+    // std::cin >> ganancia;
+    // std::cout << "Acciones del socio 1: ";
+    // std::cin >> acciones_socio_1;
+    // std::cout << "Acciones del socio 2: ";
+    // std::cin >> acciones_socio_2;
+    // std::cout << "Acciones del socio 3: ";
+    // std::cin >> acciones_socio_3;
+    // std::cout << std::endl;
+
+    // pago_por_accion = ganancia / (acciones_socio_1 + acciones_socio_2 + acciones_socio_3);
+
+    // std::cout << "La ganancia del socio 1 es: " << pago_por_accion * acciones_socio_1 << std::endl;
+    // std::cout << "La ganancia del socio 2 es: " << pago_por_accion * acciones_socio_2 << std::endl;
+    // std::cout << "La ganancia del socio 3 es: " << pago_por_accion * acciones_socio_3 << std::endl;
+
+    /*Ejercicio 5
+    Elabore un programa en C++ para una empresa de ventas de piezas de automóviles, que
+    determine el precio al que debe vender una pieza considerando un porcentaje de ganancia. Para
+    ello se leerán el precio de compra de la pieza y el porcentaje de ganancia que desea obtener la
+    empresa en tanto por ciento.*/
+
+    // float price_pieza, price_final, porcentaje_ganancia;
+    // std::cout << "Ingrese el precio de la pieza: ";
+    // std::cin >> price_pieza;
+    // std::cout << "Ingrese el porcentaje que desea ganar: ";
+    // std::cin >> porcentaje_ganancia;
+    // std::cout << std::endl;
+    // price_final = price_pieza + (price_pieza * (porcentaje_ganancia / 100));
+    // std::cout << "El precio al que se debe vender la pieza para lograr el porcentaje de ganancia es: " << price_final;
+
+    /*Ejercicio 6
+    Una tienda ha puesto en oferta la venta de un producto ofreciendo un determinado porcentaje
+    de descuento sobre el importe de la compra. Elabore un programa en C++ que determine el
+    importe de la compra, el importe del descuento y el importe a pagar por la compra de cierta
+    cantidad de unidades del producto.
+    */
+
+    return 0;
+}
