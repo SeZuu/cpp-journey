439f9e0aa265b3d691ef1a94d26909214e38847e
sezu <dukemlet@gmail.com>
2024-07-31 06:26:14 -0500

    02-exercises files created

---
+++ 01-fundamentals/fundamentals-exercises/02-exercises.cpp
@@ -0,0 +1,84 @@
+// SELECTIVE STRUCTURES EXERCISES !!!
+
+#include <iostream>
+
+int main()
+{
+
+    // 1. Par o Impar
+    // int number;
+    // std::cout << "Ingrese numero: ";
+    // std::cin >> number;
+    // std::cout << ((number % 2) == 0 ? "El numero es par." : "El numero es impar.");
+
+    // 4. Entrada al Cine
+    // int price, age;
+    // std::cout << "ingrese edad: ";
+    // std::cin >> age;
+
+    // if (age < 18)
+    //     price = 8;
+    // else if (age < 50)
+    //     price = 15;
+    // else
+    //     price = 5;
+
+    // std::cout << "El precio de la entrada es: " << price;
+
+    // 5. Es triangulo?
+    // int side_a, side_b, side_c;
+    // std::cout << "ingrese los lados: ";
+    // std::cin >> side_a;
+    // std::cin >> side_b;
+    // std::cin >> side_c;
+
+    // if ((side_a + side_b > side_c && side_a - side_b < side_c) &&
+    //     (side_a + side_c > side_b && side_a - side_c < side_b) &&
+    //     (side_c + side_b > side_a && side_c - side_b < side_a))
+    // {
+    //     std::cout << "ES UN TRIANGULO";
+    // }
+    // else
+    //     std::cout << "NO ES UN TRIANGULO";
+
+    // 6. N1 es multiplo de N2
+    // int n1, n2;
+    // std::cout << "ingrese 2 numeros enteros: " << std::endl;
+    // std::cin >> n1;
+    // std::cin >> n2;
+
+    // if (n1 % n2 == 0)
+    //     std::cout << n1 << " es multiplo de " << n2;
+    // else
+    //     std::cout << n1 << " no es multiplo de " << n2;
+
+    // char gen;
+    // int edad;
+    // float pulsaciones;
+    // std::cout << "Ingrese el genero (f o m): ";
+    // std::cin >> gen;
+    // std::cout << "Ingrese su edad: ";
+    // std::cin >> edad;
+
+    // if (gen == 'f')
+    //     pulsaciones = (220 - edad) / 10.0;
+    // else
+    //     pulsaciones = (210 - edad) / 10.0;
+
+    // std::cout << "El numero debe ser de " << pulsaciones << " pulsaciones por cada 10 sec aprox.";
+
+    // int gl, gv;
+    // std::cout << "goles local: ";
+    // std::cin >> gl;
+    // std::cout << "goles visit: ";
+    // std::cin >> gv;
+
+    // if (gl < 1 && gv < 1)
+    //     std::cout << "Partido en empate";
+    // if (gl > gv)
+    //     std::cout << "Gano el equipo local";
+    // else
+    //     std::cout << "gano el equipo visit.";
+
+        return 0;
+}
