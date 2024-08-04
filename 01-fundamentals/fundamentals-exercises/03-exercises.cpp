da59c9487ab21d23e47dfdc1a8dea5b5354ef129
sezu <dukemlet@gmail.com>
2024-08-04 16:14:52 -0500

    created and updated: 03-exercises

---
+++ 01-fundamentals/fundamentals-exercises/03-exercises.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+
+int main() {
+    // 1.
+    // int sum = 0;
+    // for (int i = 1; i < 11; i++) {
+    //     sum += i;
+    // }
+    // std::cout << "La suma de los 10 primeros numeros positivos es: " << sum;
+
+    // 2.
+    // int sum = 0;
+    // int n;
+    // std::cout << "ingrese n: ";
+    // std::cin >> n;
+    // for (int i = 0; i < n; i++) {
+    //     if (i % 2 != 0) {
+    //         sum += i;
+    //     }
+    // }
+    // std::cout << "La suma es: " << sum;
+
+    // 3.
+    // int sum = 0;
+    // int n;
+    // std::cout << "ingrese n: ";
+    // std::cin >> n;
+    // std::cout << "Los multiplos son ";
+    // for (int i = 1; i < n; i++) {
+    //     if (i % 3 == 0) {
+    //         std::cout << i << " ";
+    //     }
+    // }
+
+    // 4.
+    // int n;
+    // std::cout << "ingrese un numero (1 - 9): ";
+    // std::cin >> n;
+    // for (int i = 0; i < 13; i++) {
+    //     std::cout << n << " x " << i << " = " << n * i << std::endl;
+    // }
+
+    // 5.
+    // int n = 4;
+    // int m = 5;
+    // int sum_par = 0, sum_impar = 0;
+    // int cont = 0;
+    // int i = 2;
+    // while (cont < n) {
+    //     if (i % 2 == 0) {
+    //         sum_par += i;
+    //         cont++;
+    //     }
+    //     i++;
+    // }
+    // cont = 0;
+    // i = 1;
+    // while (cont <= n) {
+    //     if (i % 2 != 0) {
+    //         sum_impar += i;
+    //         cont++;
+    //     }
+    //     i++;
+    // }
+    // std::cout << "Sumatoria de pares: " << sum_par << std::endl;
+    // std::cout << "Sumatoria de impares: " << sum_impar;
+
+    // 6.
+    // int n;
+    // int sum = 0;
+    // std::cout << "n: ";
+    // std::cin >> n;
+    // for (int i = 0; i < n * 2; i++) {
+    //     if (i % 2 != 0) {
+    //         sum += i;
+    //     }
+    // }
+    // std::cout << "resultado: " << sum;
+
+    // 7.
+    // int n_alumnos;
+    // float ef, ep, tf, prom;
+    // std::cout << "numero de alumnos: ";
+    // std::cin >> n_alumnos;
+    // prom = 0;
+    // for (int i = 0; i < n_alumnos; i++) {
+    //     std::cout << "EF del alumno " << i + 1 << ": ";
+    //     std::cin >> ef;
+    //     std::cout << "EP del alumno " << i + 1 << ": ";
+    //     std::cin >> ep;
+    //     std::cout << "TF del alumno " << i + 1 << ": ";
+    //     std::cin >> tf;
+    //     prom = (0.55 * ef) + (0.30 * ep) + (0.15 * tf);
+    //     std::cout << "Su promedio final es: " << prom << "/n";
+    // }
+
+    // 8.
+}
